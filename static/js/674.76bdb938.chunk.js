3c0212f82a95d773e2f17bb012af3a3cf950e52b
PolinaIgrushkina <108811462+PolinaIgrushkina@users.noreply.github.com>
2023-03-07 19:25:13 +0000

    Deploying to gh-pages from @ PolinaIgrushkina/rick-and-morty@94dad983441c21e890a9b4682fea4e5ebbe4a3c7 🚀

---
+++ static/js/674.76bdb938.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrick_and_morty=self.webpackChunkrick_and_morty||[]).push([[674],{674:function(r,n,e){e.r(n),e.d(n,{default:function(){return a}});e(791);var t=e(184);function a(){return(0,t.jsx)("div",{children:"CharacterDetailsPage"})}}}]);
+//# sourceMappingURL=674.76bdb938.chunk.js.map
