620a9dd1fcb5d957822a8e657481bc08ab28102c
Polina Igrushkina <polina.igrushkina@gmail.com>
2023-03-08 22:53:31 +0300

    Added Loader

--- src/components/CharactersList/CharactersList.jsx
+++ src/components/CharactersList/CharactersList.jsx
@@ -4,10 +4,12 @@ import { useSearchParams } from 'react-router-dom';
 import { fetchCharacters } from 'helpers/API';
 import ChacartersItem from 'components/ChacartersItem/ChacartersItem';
 import css from './CharactersList.module.css';
+import Loader from 'components/Loader/Loader';
 
 export default function CharactersList() {
   const [characters, setCharacters] = useState([]);
   const [pages, setPages] = useState(0);
+  const [status, setStatus] = useState('init');
 
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -15,12 +17,19 @@ export default function CharactersList() {
   const name = searchParams.get('name') ?? '';
 
   useEffect(() => {
-    async function getAllCharacters() {
-      const res = await fetchCharacters(page, name);
-      setCharacters(res.results);
-      setPages(res.info.pages);
+    setStatus('loading');
+    try {
+      async function getAllCharacters() {
+        const res = await fetchCharacters(page, name);
+        setCharacters(res.results);
+        setPages(res.info.pages);
+        setStatus('success');
+      }
+      getAllCharacters();
+    } catch (error) {
+      setStatus('error');
+      console.log(error);
     }
-    getAllCharacters();
   }, [page, name]);
 
   const inAlphabeticalOrder = characters?.sort(
@@ -37,35 +46,42 @@ export default function CharactersList() {
 
   return (
     <>
-      <ul className={css.list}>
-        {inAlphabeticalOrder.map(character => (
-          <ChacartersItem
-            image={character.image}
-            name={character.name}
-            id={character.id}
-            species={character.species}
-            key={character.id}
-          />
-        ))}
-      </ul>
+      {status === 'init'}
+      {status === 'success' && (
+        <>
+          <ul className={css.list}>
+            {inAlphabeticalOrder.map(character => (
+              <ChacartersItem
+                image={character.image}
+                name={character.name}
+                id={character.id}
+                species={character.species}
+                key={character.id}
+              />
+            ))}
+          </ul>
 
-      <div className={css.buttonscontainer}>
-        <button
-          disabled={Number(page) === 1}
-          className={Number(page) === 1 ? css.disabled : css.button}
-          onClick={() => handleChangePage(Number(page) - 1)}
-        >
-          Prev Page
-        </button>
-        <p className={css.pageTitle}>Page {page}</p>
-        <button
-          className={Number(page) === pages ? css.disabled : css.button}
-          onClick={() => handleChangePage(Number(page) + 1)}
-          disabled={Number(page) === pages}
-        >
-          Next Page
-        </button>
-      </div>
+          <div className={css.buttonscontainer}>
+            <button
+              disabled={Number(page) === 1}
+              className={Number(page) === 1 ? css.disabled : css.button}
+              onClick={() => handleChangePage(Number(page) - 1)}
+            >
+              Prev Page
+            </button>
+            <p className={css.pageTitle}>Page {page}</p>
+            <button
+              className={Number(page) === pages ? css.disabled : css.button}
+              onClick={() => handleChangePage(Number(page) + 1)}
+              disabled={Number(page) === pages}
+            >
+              Next Page
+            </button>
+          </div>
+        </>
+      )}
+      {status === 'loading' && <Loader />}
+      {status === 'error' && <p>Error! Попробуйте найти что-то другое.</p>}
     </>
   );
 }
